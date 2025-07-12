66257504af6db1705fefc54528cc71b2dbe1dfd5
neon alex <neon.alex888@gmail.com>
2025-07-12 19:47:52 +0000

    I can still see that light green color, while the cube it switching the

--- src/components/cube/Cube.tsx
+++ src/components/cube/Cube.tsx
@@ -56,9 +56,6 @@ const Cube: React.FC<CubeProps> = ({ rotation, shapes }) => {
         <div className="cube-face cube-face-bottom">
           <CubeFaceContent face={shapes.bottom} />
         </div>
-        {/* Inner faces to make the cube look solid */}
-        <div className="cube-face cube-face-inner-left"></div>
-        <div className="cube-face cube-face-inner-right"></div>
       </motion.div>
     </div>
   );
