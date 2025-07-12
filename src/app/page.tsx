c8c7b07bd6fd3a2062c055f2c06b5bffae0aa2f1
neon alex <neon.alex888@gmail.com>
2025-07-12 14:42:57 +0000

    Now please move the 3D cube 3 rem upward

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         </p>
       </div>
       <div 
-        className="w-full flex-grow flex items-center justify-center min-h-[400px]"
+        className="w-full flex-grow flex items-center justify-center min-h-[400px] -mt-12"
       >
         <Cube rotation={rotation} shapes={shapes} />
       </div>
