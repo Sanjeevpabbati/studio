ca1ee14062fd6d936f253c3787789aa1f2b69aa6
neon alex <neon.alex888@gmail.com>
2025-07-12 20:19:15 +0000

    Yes please.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#1E1E1E" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body antialiased">
         <TouchAnimation />
